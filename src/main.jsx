48059d14d5a093f753bf11897ab071bbd092a102
Kiruba Nithi <kirubanithi812@gmail.com>
2026-02-22 12:11:15 +0530

    resolved authcontext import

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './styles/index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext.jsx';
+import { AuthProvider } from './context/AuthContext';
 import { ToastProvider } from './context/ToastContext';
 
 createRoot(document.getElementById('root')).render(
